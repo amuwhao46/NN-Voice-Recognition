205c8f0aacf935b3f6f32cc1a84643ccab3b4757
Oke Amuwha <amuwhao46@gmail.com>
2022-04-23 13:00:56 -0400

    Split samples and added alphabet to verify

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -20,3 +20,26 @@ metadata_df.columns = ["file_name", "transcription", "normalized_transcription"]
 metadata_df = metadata_df[["file_name", "normalized_transcription"]]
 metadata_df = metadata_df.sample(frac=1).reset_index(drop=True)
 metadata_df.head(3)
+
+# Split data into training and validation sets
+split = int(len(metadata_df) * 0.90)
+df_train = metadata_df[:split]
+df_val = metadata_df[split:]
+
+print(f"Size of the training set: {len(df_train)}")
+print(f"Size of the training set: {len(df_val)}")
+
+'''----===[Preprocessing]===----'''
+# The set of characters accepted in the transcription.
+characters = [x for x in "abcdefghijklmnopqrstuvwxyz'?! "]
+# Mapping characters to integers
+char_to_num = keras.layers.StringLookup(vocabulary=characters, oov_token="")
+# Mapping integers back to original characters
+num_to_char = keras.layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+print(
+    f"The vocabulary is: {char_to_num.get_vocabulary()} "
+    f"(size ={char_to_num.vocabulary_size()})"
+)
