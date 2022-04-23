0e2f6e044d9d4478fb7eb415f23fb3b7e77cdbe3
Oke Amuwha <amuwhao46@gmail.com>
2022-04-23 13:52:28 -0400

    Removed comment

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-#from tensorflow.keras import layers
+from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 from IPython import display
 from jiwer import wer
