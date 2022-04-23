10babf64cbacb8b08bce0e6052c1202ac85e1265
Oke Amuwha <amuwhao46@gmail.com>
2022-04-23 16:54:36 -0400

    Renamed import

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
+layers = tf.keras
 import matplotlib.pyplot as plt
 from IPython import display
 from jiwer import wer
