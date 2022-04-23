e49e04b5d3455e25554eadafb38616325a576a18
Oke Amuwha <amuwhao46@gmail.com>
2022-04-23 13:47:56 -0400

    Show accuracy of predictions

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -271,3 +271,24 @@ history = model.fit(
     epochs=epochs,
     callbacks=[validation_callback],
 )
+
+'''----===[Output]===----'''
+# Let's check results on more validation samples
+predictions = []
+targets = []
+for batch in validation_dataset:
+    X, y = batch
+    batch_predictions = model.predict(X)
+    batch_predictions = decode_batch_predictions(batch_predictions)
+    predictions.extend(batch_predictions)
+    for label in y:
+        label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+        targets.append(label)
+wer_score = wer(targets, predictions)
+print("-" * 100)
+print(f"Word Error Rate: {wer_score:.4f}")
+print("-" * 100)
+for i in np.random.randint(0, len(predictions), 5):
+    print(f"Target    : {targets[i]}")
+    print(f"Prediction: {predictions[i]}")
+    print("-" * 100)
