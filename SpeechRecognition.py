572f9a8bb56213a7c712d16d2a57435b14969d1f
Oke Amuwha <amuwhao46@gmail.com>
2022-04-24 13:02:06 -0400

    Imported the right library

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-layers = tf.keras
+from keras import layers
 import matplotlib.pyplot as plt
 from IPython import display
 from jiwer import wer
